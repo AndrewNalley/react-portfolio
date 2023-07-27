52a4698acf3425ab9b60d82e7acf0ac0c7dd1874
Andrew Nalley <awnalley@gmail.com>
2023-07-27 16:36:06 -0500

    reorganize navbar files

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
-import Navigation from './components/Navbar/Navigation';
-import PageContainer from './pages';
+import Navigation from './components/Navbar';
+import HomeAbout from './pages/HomeAbout';
+import Contact from './pages/Contact';
+import Project from './pages/Portfolio';
+import Resume from './pages/Resume';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <>
-      <Header />
-      <Navigation />
-      <PageContainer />
-      <Footer />
+      <Router>
+        <Header />
+        <Navigation />
+        <Routes>
+          <Route path='/' element={<HomeAbout />} />
+          <Route path='/projects' element={<Project />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/resume' element={<Resume />} />
+        </Routes>
+        <Footer />
+      </Router>
     </>
   );
 }
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,22 +1,22 @@
 import React, { useState } from "react";
 import { Nav, NavLink, NavMenu }
-    from "./NavElements";
+    from "./NavbarElements";
 
 function Navigation() {
     return (
         <>
             <Nav>
                 <NavMenu>
-                    <NavLink to="/" activeStyle>
+                    <NavLink to="/" activestyle>
                         Home
                     </NavLink>
-                    <NavLink to="/contact" activeStyle>
+                    <NavLink to="/contact" activestyle>
                         Contact
                     </NavLink>
-                    <NavLink to="/projects" activeStyle>
+                    <NavLink to="/projects" activestyle>
                         Blogs
                     </NavLink>
-                    <NavLink to="/Resume" activeStyle>
+                    <NavLink to="/resume" activestyle>
                         Resume
                     </NavLink>
                 </NavMenu>
@@ -24,5 +24,5 @@ function Navigation() {
         </>
     );
 }
-// code here
+
 export default Navigation;
