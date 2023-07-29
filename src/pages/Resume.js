bceb62ae93a31a5ff007148b40d657adcde9518f
AndrewNalley <104536377+AndrewNalley@users.noreply.github.com>
2023-07-28 23:03:48 -0500

    add google doc resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,14 +1,26 @@
 import React, { useState } from 'react';
 
 function Resume() {
+    const googleDocUrl = 'https://docs.google.com/document/d/e/2PACX-1vTgFYVw8ekGHvoLmS2altl2RAAFT1qEpMvnx-wKA0rs5Y-S1nEgFCcvmUyBEBtQR3sFmZUgvslmP0GX/pub?embedded=true';
     return (
-        <a
-            href="https://docs.google.com/document/d/1s-cFFvgDVXacxeHFOLWv4atzeLtoByBnQx--7XlKWt8/edit?usp=sharing"
-            role="button"
-            className="secondary"
-        >
-            Resume
-        </a>
+        <>
+            <div style={{ width: '100vw'}}>
+                <iframe
+                    title="Google Doc"
+                    src={googleDocUrl}
+                    width="600"
+                    height="800"
+                    style={{ display: 'flex' }}
+                ></iframe>
+            </div>
+            <a
+                href="https://docs.google.com/document/d/1s-cFFvgDVXacxeHFOLWv4atzeLtoByBnQx--7XlKWt8/edit?usp=sharing"
+                role="button"
+                className="secondary"
+            >
+                Resume
+            </a>
+        </>
     )
 }
 
