db43dba6e139b80b65eaa649496ed283b105c767
Andrew Nalley <awnalley@gmail.com>
2023-08-02 14:48:25 -0500

    add 404 catch-all

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import './App.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import Header from './components/Header';
@@ -9,11 +10,13 @@ import Contact from './pages/Contact';
 import Project from './pages/Portfolio';
 import Resume from './pages/Resume';
 import Footer from './components/Footer';
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
     <>
       <Router basename='/react-portfolio'>
+
         <Header />
         <Navigation />
         <Routes>
@@ -21,8 +24,10 @@ function App() {
           <Route path='/projects' element={<Project />} />
           <Route path='/contact' element={<Contact />} />
           <Route path='/resume' element={<Resume />} />
+          <Route path='*' element={<PageNotFound />} />
         </Routes>
         <Footer />
+
       </Router>
     </>
   );
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,19 +7,44 @@ const Contact = () => {
         message: '',
     });
 
+    const [errors, setErrors] = useState({
+        name: '',
+        email: '',
+        message: '',
+      });
+
     const handleChange = (e) => {
         const { name, value } = e.target;
         setFormData({ ...formData, [name]: value });
+        setErrors({ ...errors, [name]: '' });
     };
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        if (formData.name && formData.email && formData.message) {
-            // Perform your submission logic here (e.g., send data to the server)
-            alert('Form submitted successfully!');
-        } else {
-            alert('Please fill in all fields before submitting.');
-        }
+        let newErrors = {};
+
+        if (!formData.name.trim()) {
+            newErrors.name = 'Name is required';
+          }
+          if (!formData.email.trim()) {
+            newErrors.email = 'Email is required';
+          }
+          if (!formData.message.trim()) {
+            newErrors.message = 'Message is required';
+          }
+
+          const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (formData.email.trim() && !emailRegex.test(formData.email)) {
+      newErrors.email = 'Invalid email format';
+    }
+
+
+         if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors);
+    } else {
+      // Perform your submission logic here (e.g., send data to the server)
+      alert('Form submitted successfully!');
+    }
     };
 
     return (
@@ -35,7 +60,9 @@ const Contact = () => {
                             name="name"
                             value={formData.name}
                             onChange={handleChange}
+                            onBlur={handleChange}
                         />
+                        {errors.name && <span className="error">{errors.name}</span>}
                     </div>
                     <div>
                         <label htmlFor="email">Email:</label>
@@ -45,7 +72,9 @@ const Contact = () => {
                             name="email"
                             value={formData.email}
                             onChange={handleChange}
+                            onBlur={handleChange}
                         />
+                        {errors.email && <span className="error">{errors.email}</span>}
                     </div>
                     <div>
                         <label htmlFor="message">Message:</label>
@@ -54,7 +83,9 @@ const Contact = () => {
                             name="message"
                             value={formData.message}
                             onChange={handleChange}
+                            onBlur={handleChange}
                         />
+                        {errors.message && <span className="error">{errors.message}</span>}
                     </div>
                     <button type="submit">Submit</button>
                 </form>
