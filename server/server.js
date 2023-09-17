31cc5fb0ed8235c06162a4cbbc26bf533a718875
AndrewNalley <104536377+AndrewNalley@users.noreply.github.com>
2023-09-16 22:44:30 -0500

    create basic server file structure

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware to parse JSON data
+app.use(express.json());
+
+// Define routes and handle form submissions here
+
+app.post('/api/contact', (req, res) => {
+    const { name, email, subject, message } = req.body;
+  
+    // Validate form data and send emails using Nodemailer
+    // Ensure to handle errors and send appropriate responses
+  });
+  
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
