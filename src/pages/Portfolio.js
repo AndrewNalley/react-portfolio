088e533a090e17e0ced9fa4e8ba7b02a512e4b23
AndrewNalley <104536377+AndrewNalley@users.noreply.github.com>
2023-10-09 21:39:03 -0500

    entire project divs are clickable link

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -7,63 +7,57 @@ import pokemonGame from '../images/pokemon-game.png';
 import logoMaker from '../images/logo-maker.png';
 import noteTaker from '../images/note-taker.png';
 import portfolioPhoto from '../images/portfolio.png';
+const proj1Link = "https://whispering-crag-58704-755d34513186.herokuapp.com/";
+const proj2Link = "https://github.com/AndrewNalley/employee-tracker";
+const proj3Link = "https://andrewnalley.github.io/weather-dashboard/";
+const proj4Link = "https://2meng.github.io/Whos-that-pokemon/";
+const proj5Link = "https://github.com/AndrewNalley/logo-maker";
+const proj6Link = "https://aqueous-springs-29735-51eb1397bb8e.herokuapp.com/";
+const proj7Link = "https://github.com/AndrewNalley/react-portfolio";
+
 
 function Portfolio() {
     return (
         <>
-            <h4 className="heading-center">Portfolio</h4>
-            <article className="grid">
-                <div id='proj-1'>
+            <h4 className='heading-center'>Portfolio</h4>
+            <article className='grid'>
+                <a id='proj-1' href={proj1Link}>
+                    <h5>Dreamscape</h5>
                     <img src={dreamscape} alt='dreamscape' />
-                    <div className="project-title"><a href="https://whispering-crag-58704-755d34513186.herokuapp.com/">Dreamscape</a>
-                        <p>Create calming and inspirational environments</p>
-                    </div>
-                </div>
-                <div id='proj-2'>
+                    <p>Create calming and inspirational environments</p>
+                </a>
+                <a id='proj-2' href={proj2Link}>
+                    <h5>Employee Tracker</h5>
                     <img src={employeeTracker} alt='employee-tracker' />
-                    <div className="project-title"><a href="https://github.com/AndrewNalley/employee-tracker">Employee
-                        Tracker</a>
-                        <p>Command-line employee tracking application</p>
-                    </div>
-                </div>
-                <div id='proj-3'>
+                    <p>Command-line employee tracking application</p>
+                </a>
+                <a id='proj-3' href={proj3Link}>
+                    <h5>Weather Dashboard</h5>
                     <img src={weatherDashboard} alt='weather-dashboard' />
-                    <div className="project-title"><a href="https://andrewnalley.github.io/weather-dashboard/">Weather
-                        Dashboard</a>
-                        <p>Five-day weather forecast using two different APIs</p>
-                    </div>
-                </div>
+                    <p>Five-day weather forecast using two different APIs</p>
+                </a>
             </article>
-            <article className="grid">
-                <div id='proj-4'>
+            <article className='grid'>
+                <a id='proj-4' href={proj4Link}>
+                    <h5>Who's that Pokémon?!</h5>
                     <img src={pokemonGame} alt='pokemon-game' />
-                    <div className="project-title"><a href="https://2meng.github.io/Whos-that-pokemon/">Who's
-                        that Pokémon?!</a>
-                        <p>Test your pokémon knowledge!</p>
-                    </div>
-                </div>
-                <div id='proj-5'>
+                    <p>Test your pokémon knowledge!</p>
+                </a>
+                <a id='proj-5' href={proj5Link}>
+                    <h5>Logo Maker</h5>
                     <img src={logoMaker} alt='logo-maker' />
-                    <div className="project-title"><a href="https://github.com/AndrewNalley/logo-maker">Logo
-                        Maker</a>
-                        <p>Simple logos are so hot right now. Quickly generate an SVG logo!</p>
-                    </div>
-                </div>
-                <div id='proj-6'>
+                    <p>Simple logos are so hot right now. Quickly generate an SVG logo!</p>
+                </a>
+                <a id='proj-6' href={proj6Link}>
+                    <h5>Note Taker</h5>
                     <img src={noteTaker} alt='note-taker' />
-                    <div className="project-title"><a
-                        href="https://aqueous-springs-29735-51eb1397bb8e.herokuapp.com/">Note
-                        Taker</a>
-                        <p>Use to write and save notes</p>
-                    </div>
-                </div>
-                <div id='proj-7'>
+                    <p>Use to write and save notes</p>
+                </a>
+                <a id='proj-7' href={proj7Link}>
+                    <h5>Portfolio Repository</h5>
                     <img src={portfolioPhoto} alt='portfolio' />
-                    <div className="project-title"><a
-                        href="https://github.com/AndrewNalley/react-portfolio">Portfolio Repository</a>
-                        <p>See the code used for this webpage!</p>
-                    </div>
-                </div>
+                    <p>See the code used for this webpage!</p>
+                </a>
             </article>
         </>
     )
